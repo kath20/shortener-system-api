be7486366113bd0e0142710a5418b370eec6339b
Katherine Granados <kathgmasis@gmail.com>
2021-04-14 10:38:16 -0600

    feat: es6 installation
    complete installation and change the type of variables to const and let.

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,10 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* POST users login. */
 router.post("/", function (req, res, next) {
-  var userName = "admin";
-  var password = "123";
+  const userName = "admin";
+  const password = "123";
   if (req.body.userName === userName && req.body.password === password) {
     res.status(200).send("respond with a resource LOGIN");
   } else {
